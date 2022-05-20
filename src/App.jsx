bce21427a0aaff73ed230407f31f497b60fd5178
Mylena Vitoriano <71454293+mylenavitoriano@users.noreply.github.com>
2022-05-19 21:31:47 -0300

    Primeiro commit. Projeto funcional

---
+++ src/App.jsx
@@ -0,0 +1,46 @@
+import { useEffect, useState } from "react"
+import './styles/style.scss'
+import { FaDice, FaSearch } from 'react-icons/fa'
+
+export default function App() {
+
+  const [search, setSearch] = useState('')
+  const [data, setData] = useState({})
+
+  const buscaPalavra = async () => {
+    const req = await fetch(`https://api.adviceslip.com/advice/search/${search}`)
+    const dados = await req.json()
+    setData(dados.slips[Math.floor(Math.random() * dados.slips.length)])
+  }
+
+  const buscaId = async () => {
+    let numberRandom = Math.floor(Math.random() * 224)
+    const req = await fetch(`https://api.adviceslip.com/advice/${numberRandom}`)
+    const dados = await req.json()
+    setData(dados.slip)
+  }
+
+  return (
+    <div>
+      <div className={'container'}>
+        <div className={'card'}>
+          <div className={'container-card'}>
+            <div className={'header'}>
+              <div className={'dice'} onClick={buscaId}>
+                <FaDice color={"#FFF"}/>
+              </div>
+              <div className={'search'}>
+                <input type="text" onChange={(e)=>setSearch(e.target.value)} placeholder="Search by keyword"/>
+                <button onClick={buscaPalavra}><FaSearch color={"#FFF"}/></button>
+              </div>
+            </div>
+            <div className={data.advice ? 'advice' : 'hidden'}>
+              <p>{data.advice}</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
